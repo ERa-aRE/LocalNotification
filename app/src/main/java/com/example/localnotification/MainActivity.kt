68a460439e9b4d72148602447c3de12376e3ad06
Eradaviraf <erarebornsame@gmail.com>
2023-01-26 15:25:00 +0330

    number picker library is working

--- app/src/main/java/com/example/localnotification/MainActivity.kt
+++ app/src/main/java/com/example/localnotification/MainActivity.kt
@@ -1,18 +1,18 @@
 package com.example.localnotification
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.work.*
+import com.chargemap.compose.numberpicker.ListItemPicker
+import com.chargemap.compose.numberpicker.NumberPicker
 import com.example.localnotification.domain.ArbitraryInfo
 import com.example.localnotification.presentaion.CurvedScrollView
 import com.example.localnotification.ui.theme.LocalNotificationTheme
@@ -20,9 +20,11 @@ import java.util.concurrent.TimeUnit
 
 class MainActivity : ComponentActivity() {
 
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         dateCheckerFunction()
         super.onCreate(savedInstanceState)
+
         val service = NotificationService(applicationContext)
         val menuList =
             listOf(
@@ -43,13 +45,91 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     //Greeting(service)
-                    CurvedScrollView(menuList) { eachRoute ->
+                    /*CurvedScrollView(menuList) { eachRoute ->
                         cardView(text = eachRoute)
 
-                    }
+                    }*/
+                    numberAndListCard()
                 }
             }
         }
+    }
+
+    @Composable
+    private fun numberAndListCard() {
+        var day by remember {
+            mutableStateOf(0)
+        }
+        var month by remember {
+            mutableStateOf(0)
+        }
+        var year by remember {
+            mutableStateOf(0)
+        }
+        val persianMonth = listOf(
+            "فروردین",
+            "اردیبهشت",
+            "خرداد",
+            "تیر",
+            "مرداد",
+            "شهریور",
+            "مهر",
+            "آبان",
+            "آذر",
+            "دی",
+            "بهمن",
+            "اسفند"
+        )
+        var pickerValue by remember { mutableStateOf(15) }
+        var pickerValue2 by remember { mutableStateOf(1390) }
+        var state by remember { mutableStateOf(persianMonth[0]) }
+        Row(modifier= Modifier
+            .fillMaxWidth()
+            .padding(20.dp)) {
+
+
+            NumberPicker(
+                value = pickerValue,
+                range = 1..31,
+                onValueChange = {
+                    pickerValue = it
+                    day =it
+                }
+            )
+            ListItemPicker(
+                label = { it },
+                value = state,
+                onValueChange =
+                { state = it
+                when(it){
+                    "فروردین"-> month=1
+                    "اردیبهشت"->month=2
+                    "خرداد"->month=3
+                    "تیر"->month=4
+                    "مرداد"->month=5
+                    "شهریور"->month=6
+                    "مهر"->month=7
+                    "آبان"->month=8
+                    "آذر"->month=9
+                    "دی"->month=10
+                    "بهمن"->month=11
+                    "اسفند"->month=12
+
+                }                },
+                list = persianMonth
+            )
+            NumberPicker(
+                value = pickerValue2,
+                range = 1300..1450,
+                onValueChange = {
+                    pickerValue2 = it
+                    year = it
+                }
+            )
+            Text(text = "year=$year,month=$month,day=$day")
+
+        }
+
     }
 
     @Composable
@@ -57,6 +137,8 @@ class MainActivity : ComponentActivity() {
         Column(modifier = Modifier.padding(8.dp), verticalArrangement = Arrangement.Center) {
             Text(text = text)
         }
+
+
     }
 
 
